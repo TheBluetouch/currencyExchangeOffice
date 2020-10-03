0f7b1ebe9303abe09eda02f25067abc0794161e8
Bluetouch <bogusz28@wp.pl>
2020-10-03 12:43:56 +0200

    Init commit

---
+++ src/main/java/com/kacperBogusz/CurrencyExchangeOffice/CurrencyExchangeOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.kacperBogusz.CurrencyExchangeOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kacperBogusz/CurrencyExchangeOffice/CurrencyExchangeOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kacperBogusz.CurrencyExchangeOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
